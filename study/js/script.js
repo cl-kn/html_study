cc853a859907ba1e0edfb5a7131460d4185defcf
cl-kn <72800355+cl-kn@users.noreply.github.com>
2021-04-24 22:46:52 +0900

    add folder [study]

---
+++ study/js/script.js
@@ -0,0 +1,34 @@
+currentItem = $(".current-item");
+if (currentItem[0]) {
+  currentItem.css({
+    width: currentItem.width(),
+    left: currentItem.position().left,
+  });
+}
+
+$("#navigation li").hover(
+  // ホバー時
+  function () {
+    // ホバーしているメニューの幅と左位置に変更
+    $("#slide-line").css({
+      width: $(this).width(),
+      left: $(this).position().left,
+    });
+  },
+  // ホバーが外れたとき
+  function () {
+    // カレントメニューがある場合
+    if (currentItem[0]) {
+      // カレントメニューに下線を戻す
+      $("#slide-line").css({
+        width: currentItem.width(),
+        left: currentItem.position().left,
+      });
+    }
+    // カレントメニューがない場合
+    else {
+      // 下線を消す
+      $("#slide-line").width(0);
+    }
+  }
+);
